1a438f3fa7e49bcd2f4d3b2e28690df5245a14a0
gonzadpp-dev <gonzalodavid.pp@gmail.com>
2026-06-03 15:16:59 -0600

    add exercise asteci project

---
+++ exerciseasteci/src/main/java/com/gonzadpp/exerciseasteci/ExerciseasteciApplication.java
@@ -0,0 +1,13 @@
+package com.gonzadpp.exerciseasteci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseasteciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseasteciApplication.class, args);
+	}
+
+}
---
+++ exerciseasteci/src/test/java/com/gonzadpp/exerciseasteci/ExerciseasteciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gonzadpp.exerciseasteci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseasteciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
